c9397b206cc4fdc263de1622eb6afbb19424adb4
Kenneth Murerwa <xwaxes@gmail.com>
2023-01-04 10:18:03 +0300

    feat: Updated the coroutines dispatcher rule

--- app/src/test/java/com/murerwa/bbscorecounter/utils/BaseUnitTest.kt
+++ app/src/test/java/com/murerwa/bbscorecounter/utils/BaseUnitTest.kt
@@ -2,11 +2,13 @@ package com.murerwa.bbscorecounter.utils
 
 import org.junit.Rule
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
+@ExperimentalCoroutinesApi
 open class BaseUnitTest {
 
     @get:Rule
-    val coroutinesTestRule = MainCoroutineScopeRule()
+    val coroutinesTestRule = MainDispatcherRule()
 
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
